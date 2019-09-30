8eac9dfcef85ea1886a8ebc29e068a7530ac7494
KHong <albtrossfacefall@gmail.com>
2019-09-30 14:26:27 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+     actors.map {|c|c.full_name}
+  end
+
+   def build_network(call_letters:)
+      self.network=Network.new(call_letters: call_letters)
+   end
 end
