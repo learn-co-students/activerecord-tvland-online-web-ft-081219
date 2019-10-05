a6b58d054b011fb707fa7aea128cd32949e58559
David Ingram <david@ingramaviation.com>
2019-10-05 17:14:52 -0500

    Done.

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,8 +2,8 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
     def change
       create_table :characters do |t|
         t.string :name
-        t.actor_id :integer
-        t.show_id :integer
+        t.integer :actor_id
+        t.integer :show_id
       end
     end
 end
